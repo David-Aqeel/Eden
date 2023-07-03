523961fb20d640f17cf6ba14baf9344bc9370d75
DavidAqeel <qbkid123@gmail.com>
2023-07-03 06:30:40 -0400

    shopping cart details

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,20 +1,69 @@
-export default function NewOrderPage() {
-    return ( 
-        <main className="NewOrderPage">
-            <aside>
-                <Logo />
-                <CategoryList
-                categories={categoriesRef.current}
-                activeCat={activeCat}
-                setActiveCat={setActiveCat}
-                />
-                <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
-                <UserLogOut user={user} setUser={setUser} />
-            </aside>
-            <MenuList
-                menuItems={menuItems.filter(item => item.category.name === activeCat)}
-            />
-            <OrderDetail />
-        </main>
-    )
+import { useState, useEffect, useRef } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+import * as ordersAPI from '../../utilities/orders-api';
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function NewOrderPage({ user, setUser }) {
+  const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
+  const [cart, setCart] = useState(null);
+  const categoriesRef = useRef([]);
+
+  // The empty dependency array causes the effect
+  // to run ONLY after the FIRST render
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      categoriesRef.current = [...new Set(items.map(item => item.category.name))];
+      setMenuItems(items);
+      setActiveCat(categoriesRef.current[0]);
+    }
+    getItems();
+
+    // Load cart (a cart is the unpaid order for the logged in user)
+    async function getCart() {
+      const cart = await ordersAPI.getCart();
+      setCart(cart);
+    }
+    getCart();
+  }, []);
+
+    /*--- Event Handlers --- */
+  async function handleAddToOrder(itemId) {
+    // Baby step
+    // alert(`add item: ${itemId}`);
+    // 1. Call the addItemToCart function in ordersAPI, passing to it the itemId, and assign the resolved promise to a variable named cart.
+    const updatedCart = await ordersAPI.addItemToCart(itemId);
+    // 2. Update the cart state with the updated cart received from the server
+    
+
+  }
+
+  
+
+  return (
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        {/* <UserLogOut user={user} setUser={setUser} /> */}
+      </aside>
+      <MenuList 
+        handleAddToOrder={handleAddToOrder}
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail order={cart} />
+    </main>
+  );
 }
